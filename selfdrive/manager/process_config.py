26bb77ab11899c2124853d13a1f045b096f27fd0
khoi <git@khoi.io>
2022-04-29 10:45:42 +0700

    notifyd

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -36,6 +36,7 @@ procs = [
   PythonProcess("updated", "selfdrive.updated", enabled=not PC, onroad=False, offroad=True),
   PythonProcess("uploader", "selfdrive.loggerd.uploader", offroad=True),
   PythonProcess("statsd", "selfdrive.statsd", offroad=True),
+  PythonProcess("notifyd", "selfdrive.notifyd", offroad=True),
 
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], onroad=False, notcar=True),
   PythonProcess("webjoystick", "tools.joystick.web", onroad=False, notcar=True),
