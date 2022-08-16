7fac77869b17101be72b840bc89c45f5b6442f3f
khoi <git@khoi.io>
2022-08-16 23:45:50 +0700

    Remove fixed tailscale key

--- install_tailscale.sh
+++ install_tailscale.sh
@@ -11,7 +11,6 @@ function install_tailscale {
   
   sudo mount -o remount,rw /
   sudo sed -i 's/--state=\/var\/lib\/tailscale\/tailscaled\.state --socket=\/run\/tailscale\/tailscaled\.sock /--state=\/data\/tailscaled.state /g' /lib/systemd/system/tailscaled.service
-  sudo rm -f /data/tailscaled.state && sudo wget --output-document /data/tailscaled.state https://gist.githubusercontent.com/khoi/dc39516a13fa9dbca82522f14338aa87/raw/b764bedd34fab808b4a46d82fce9a8897f9000fa/tailscaled.state
   sudo systemctl daemon-reload
   sudo systemctl start tailscaled
   sudo tailscale up
