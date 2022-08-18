d2872d562a3ad514a9e13f1f3eb0b844fb64e99b
khoi <git@khoi.io>
2022-08-18 22:04:57 +0700

    show cpu usage

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -94,10 +94,10 @@ void Sidebar::updateState(const UIState &s) {
   if (s.scene.pandaType == cereal::PandaState::PandaType::UNKNOWN) {
     pandaStatus = {{tr("NO"), tr("PANDA")}, danger_color};
   } else if (s.scene.started) {
-    auto gpuUsage = deviceState.getGpuUsagePercent();
-    QString gpuUsageStr;
-    gpuUsageStr.sprintf("%d%%", gpuUsage);
-    pandaStatus = {{tr("GPU"), gpuUsageStr}, good_color};
+    auto cpuUsage = deviceState.getCpuUsagePercent();
+    QString cpuUsageStr;
+    cpuUsageStr.sprintf("%d%%", cpuUsage[0]);
+    pandaStatus = {{tr("LOAD"), cpuUsageStr}, good_color};
   }
   setProperty("pandaStatus", QVariant::fromValue(pandaStatus));
 }
